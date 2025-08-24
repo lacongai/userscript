ccc6fe6a711ac6c4328fe869bbbefca54ab2d87a
bypass-api <thailanfreefire2000@gmail.com>
2025-08-24 14:47:01 +0700

    Add files via upload

---
+++ tampermonkey.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Bypass Yeumoney (Tool)
+// @namespace    http://tampermonkey.net/
+// @version      18/07/2025
+// @description  Auto vượt yeumoney không cần chạm tay
+// @author       By R2lx
+// @require      https://raw.githubusercontent.com/lacongai/userscript/refs/heads/main/yeumoney.js
+// @match        https://yeumoney.com/*
+// @exclude      https://yeumoney.com/quangly/*
+// @icon         
+// ==/UserScript==
